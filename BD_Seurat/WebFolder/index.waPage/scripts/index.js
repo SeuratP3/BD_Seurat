30f55ff7469c834f30c438775d06fe06ad1cf2b8
Your_Wakanda_Developer_Name_Here <WakandaDeveloper@wakanda.org>
2014-06-30 15:57:11 +0200

    Update #3

---
+++ BD_Seurat/WebFolder/index.waPage/scripts/index.js
@@ -0,0 +1,19 @@
+﻿
+WAF.onAfterInit = function onAfterInit() {// @lock
+
+// @region namespaceDeclaration// @startlock
+	var combobox1 = {};	// @combobox
+// @endregion// @endlock
+
+// eventHandlers// @lock
+
+	combobox1.change = function combobox1_change (event)// @startlock
+	{// @endlock
+		// Add your code here
+		
+	};// @lock
+
+// @region eventManager// @startlock
+	WAF.addListener("combobox1", "change", combobox1.change, "WAF");
+// @endregion
+};// @endlock
