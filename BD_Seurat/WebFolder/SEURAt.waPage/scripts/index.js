f1fa4b6f052635268a71e55c10c703f82c473a30
Your_Wakanda_Developer_Name_Here <WakandaDeveloper@wakanda.org>
2014-07-02 18:00:31 +0200

    Update #13
    affichage fini
    reste principalement images et recherche à gérer

--- BD_Seurat/WebFolder/SEURAt.waPage/scripts/index.js
+++ BD_Seurat/WebFolder/SEURAt.waPage/scripts/index.js
@@ -2,11 +2,43 @@
 WAF.onAfterInit = function onAfterInit() {// @lock
 
 // @region namespaceDeclaration// @startlock
+	var button4 = {};	// @button
+	var button3 = {};	// @button
 	var button1 = {};	// @button
 	var textMarque = {};	// @textField
 // @endregion// @endlock
 
 // eventHandlers// @lock
+
+	button4.click = function button4_click (event)// @startlock
+	{// @endlock
+		// Add your code here
+		var $contOrgAv = $("#contOrgAv");
+		$contOrgAv.show();
+		var $contOrgAr = $("#contOrgAr");
+		$contOrgAr.hide();
+		//Bouton4 devient actuel (bleu ? rouge ?)
+		var $button4 = $("#button4");
+		$button4.css('background','#aaaaff');
+		//bouton3 redevient blanc
+		var $button3 = $("#button3");
+		$button3.css('background','#F0F0F0');
+	};// @lock
+
+	button3.click = function button3_click (event)// @startlock
+	{// @endlock
+		// Add your code here
+		var $contOrgAv = $("#contOrgAv");
+		$contOrgAv.hide();
+		var $contOrgAr = $("#contOrgAr");
+		$contOrgAr.show();
+		//Bouton3 devient actuel (bleu ? rouge ?)
+		var $button3 = $("#button3");
+		$button3.css('background','#aaaaff');
+		//bouton4 redevient blanc
+		var $button4 = $("#button3");
+		$button4.css('background','#F0F0F0');
+	};// @lock
 
 	button1.click = function button1_click (event)// @startlock
 	{// @endlock
@@ -25,6 +57,8 @@ WAF.onAfterInit = function onAfterInit() {// @lock
 	};// @lock
 
 // @region eventManager// @startlock
+	WAF.addListener("button4", "click", button4.click, "WAF");
+	WAF.addListener("button3", "click", button3.click, "WAF");
 	WAF.addListener("button1", "click", button1.click, "WAF");
 	WAF.addListener("textMarque", "change", textMarque.change, "WAF");
 // @endregion
