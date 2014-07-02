9e21b45f2bf30d9bceb16429f542f90c14cf11dc
Your_Wakanda_Developer_Name_Here <WakandaDeveloper@wakanda.org>
2014-07-02 11:29:32 +0200

    Update #9
    Modèle complet, mise en page avancée

---
+++ BD_Seurat/WebFolder/SEURAt.waPage/scripts/index.js
@@ -0,0 +1,31 @@
+﻿
+WAF.onAfterInit = function onAfterInit() {// @lock
+
+// @region namespaceDeclaration// @startlock
+	var button1 = {};	// @button
+	var textMarque = {};	// @textField
+// @endregion// @endlock
+
+// eventHandlers// @lock
+
+	button1.click = function button1_click (event)// @startlock
+	{// @endlock
+		var m = sources.montage.newEntity();
+		sources.fiche.avant = m;
+		
+	};// @lock
+
+	textMarque.change = function textMarque_change (event)// @startlock
+	{// @endlock
+		// Add your code here
+		
+		//var q = queryString;
+    	//sources.fiche.query(q);
+    	
+	};// @lock
+
+// @region eventManager// @startlock
+	WAF.addListener("button1", "click", button1.click, "WAF");
+	WAF.addListener("textMarque", "change", textMarque.change, "WAF");
+// @endregion
+};// @endlock
