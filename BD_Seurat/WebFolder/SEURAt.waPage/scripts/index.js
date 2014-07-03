3710c06162ffc07a46da176ee3e86a6e047cff8f
Your_Wakanda_Developer_Name_Here <WakandaDeveloper@wakanda.org>
2014-07-03 13:44:54 +0200

    Update #14
    Avant Arrière séparés OK
    a faire : copie, image, recherche avant arrière...

--- BD_Seurat/WebFolder/SEURAt.waPage/scripts/index.js
+++ BD_Seurat/WebFolder/SEURAt.waPage/scripts/index.js
@@ -2,6 +2,7 @@
 WAF.onAfterInit = function onAfterInit() {// @lock
 
 // @region namespaceDeclaration// @startlock
+	var textRecherche = {};	// @textField
 	var button4 = {};	// @button
 	var button3 = {};	// @button
 	var button1 = {};	// @button
@@ -10,34 +11,66 @@ WAF.onAfterInit = function onAfterInit() {// @lock
 
 // eventHandlers// @lock
 
+		var $contOrgAv = $("#contOrgAv");
+		var $contOrgAr = $("#contOrgAr");
+		var $button4 = $("#button4");
+		var $button3 = $("#button3");
+        var txtSrch = "";
+
+	textRecherche.keyup = function textRecherche_keyup (event)// @startlock
+	{// @endlock
+		// Add your code here
+		//var $input_content = ($textRecherche.text());
+        
+        //$("#texteRecherche").getValue();
+
+		
+		var txtR = $$("textRecherche").getValue();
+		
+        if(txtR!= ""){
+	   		var lst = txtR.split(" ");
+	   		
+	  		txtSrch = "(marque = '*"+lst[0]+"*') || (modele = '*"+lst[0]+"*') || (cylindree = "+lst[0]+") || (annee = "+lst[0]+") || (nom = '*"+lst[0]+"*')";
+	   		if(lst.length > 1){
+	   	 		for(var i=1; i<lst.length; i++){
+	     			if(lst[i] != "")
+	      			txtSrch += " || (marque = '*"+lst[i]+"*') || (modele = '*"+lst[i]+"*') || (cylindree = '*"+lst[i]+"*') || (annee = '*"+lst[i]+"*') || (nom = '*"+lst[i]+"*')";
+	    		}
+	   		} 
+	   		//$$("tInfos").setValue("Filtre : " + txtSrch);
+ 	 	}
+ 	 	else
+ 	 	{txtSrch="";}
+  		//WAF.sources.fiche.query(txtSrch); 
+        
+        //var txtSrch = "(marque=textRecherche) || (modele=textRecherche) || (";
+	 
+	  WAF.sources.fiche.query(txtSrch);
+	  //WAF.DataSource.fiche.query(txtSrch);
+	   
+	};// @lock
+
+		
 	button4.click = function button4_click (event)// @startlock
 	{// @endlock
 		// Add your code here
-		var $contOrgAv = $("#contOrgAv");
 		$contOrgAv.show();
-		var $contOrgAr = $("#contOrgAr");
 		$contOrgAr.hide();
 		//Bouton4 devient actuel (bleu ? rouge ?)
-		var $button4 = $("#button4");
-		$button4.css('background','#aaaaff');
+		//$button4.css('background','#aaaaff');
 		//bouton3 redevient blanc
-		var $button3 = $("#button3");
-		$button3.css('background','#F0F0F0');
+		//$button3.css('background','#F0F0F0');
 	};// @lock
 
 	button3.click = function button3_click (event)// @startlock
 	{// @endlock
 		// Add your code here
-		var $contOrgAv = $("#contOrgAv");
 		$contOrgAv.hide();
-		var $contOrgAr = $("#contOrgAr");
 		$contOrgAr.show();
 		//Bouton3 devient actuel (bleu ? rouge ?)
-		var $button3 = $("#button3");
-		$button3.css('background','#aaaaff');
+		//$button3.css('background','#08a808');
 		//bouton4 redevient blanc
-		var $button4 = $("#button3");
-		$button4.css('background','#F0F0F0');
+		//$button4.css('background','#F0F0F0');
 	};// @lock
 
 	button1.click = function button1_click (event)// @startlock
@@ -57,6 +90,7 @@ WAF.onAfterInit = function onAfterInit() {// @lock
 	};// @lock
 
 // @region eventManager// @startlock
+	WAF.addListener("textRecherche", "keyup", textRecherche.keyup, "WAF");
 	WAF.addListener("button4", "click", button4.click, "WAF");
 	WAF.addListener("button3", "click", button3.click, "WAF");
 	WAF.addListener("button1", "click", button1.click, "WAF");
